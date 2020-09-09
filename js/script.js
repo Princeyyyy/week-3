fe8103d7b251f92f07f2dd7e10e768ccb531c971
princemumo80@gmail.com <princemumo80@gmail.com>
2020-09-09 15:44:53 +0300

    added hover effect to portfolio and styled the forms

--- js/script.js
+++ js/script.js
@@ -35,15 +35,67 @@ $(document).ready(function () {
   });
 });
 
+//Porfolio Hover//
+$(document).ready(function(){
+  $("#work1").mouseover(function(){
+    $("#overlap").show();
+  }).mouseout(function(){
+    $("#overlap").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work2").mouseover(function(){
+    $("#overlap2").show();
+  }).mouseout(function(){
+    $("#overlap2").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work3").mouseover(function(){
+    $("#overlap3").show();
+  }).mouseout(function(){
+    $("#overlap3").hide();
+  });
+});
+$(document).ready(function(){
+  $("#work4").mouseover(function(){
+    $("#overlap4").show();
+  }).mouseout(function(){
+    $("#overlap4").hide();
+  });
+});
+
+$(document).ready(function(){
+  $("#work5").mouseover(function(){
+    $("#overlap5").show();
+  }).mouseout(function(){
+    $("#overlap5").hide();
+  });
+  $("#work6").mouseover(function(){
+    $("#overlap6").show();
+  }).mouseout(function(){
+    $("#overlap6").hide();
+  });
+  $("#work7").mouseover(function(){
+    $("#overlap7").show();
+  }).mouseout(function(){
+    $("#overlap7").hide();
+  });
+  $("#work8").mouseover(function(){
+    $("#overlap8").show();
+  }).mouseout(function(){
+    $("#overlap8").hide();
+  });
+});
 // Feedback to the customer//
 $("button").click(function (review) {
-    var customer = document.getElementById('nameDetail').value;
-    alert(' Dear ' + ' esteemed ' + customer + ' we have sucessfully received your message. ' + ' Feel free to reach out to us anytime.');
-    review.preventDefault();
+  var customer = document.getElementById('nameDetail').value;
+  alert(' Dear ' + ' esteemed ' + customer + ' we have sucessfully received your message. ' + 'Feel free to reach out to us anytime.');
+  review.preventDefault();
+});
+//Reset my form button//
+$("button").on('click', function () {
+  $('form').each(function () {
+    this.reset();
   });
-  //Reset my form button//
-  $("button").on('click', function () {
-    $('form').each(function () {
-      this.reset();
-    });
-  });
+});
